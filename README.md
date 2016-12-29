b6857c32b99d6ea89c38745a202dd9ba0c3cf6b1
David Trapp <david@dotlabs.co>
2016-12-29 14:17:03 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # wsl_node_network_interfaces_shim
-A shim to prevent node from crashing when running in WSL when os.networkInterfaces is used
+_A shim to prevent node from crashing when running in WSL when os.networkInterfaces is used_
+
+The Windows Subsystem for Linux (WSL) is great. However, it still has some issues. One of them is that network interface management isn't implemented yet.
+Because of that, the `os.networkInterfaces` function in node.js will crash with `EINVAL`. Here is the [GitHub issue about this problem](https://github.com/Microsoft/BashOnWindows/issues/468).
+
+## Workaround
+
+**TL;DR**: A quick fix for this problem would be running the following command: `curl https://raw.githubusercontent.com/CherryDT/wsl_node_network_interfaces_shim/master/wsl_node_network_interfaces_shim.sh > /tmp/wslfix.sh && chmod +x /tmp/wslfix.sh && sudo /tmp/wslfix.sh install`
+If this fails with an error, read below.
+
+-----
+
+My current workaround is shimming the `os.networkInterfaces` function by shimming node to load my shim file... ;) I'm replacing the `/usr/bin/node` symlink to link to an intermediate script file which invokes the real node with an extra `-r` argument to require a JS file at start, in which I'm replacing the `os.networkInterfaces` function to return a loopback interface.
+
+Basically, this shim will prevent the crash when any node script calls `os.networkInterfaces()`. It will not make the network interfaces work correctly, though - the scripts will always just see the loopback interface, but no actual interfaces. Still, this solves a lot of problems already.
+
+I created a bash script which does the dirty work for you.
+
+* Make sure you have node installed [managed by `n`](https://github.com/tj/n), if not: `sudo npm install -g n && sudo n latest` (instead of `latest`, you can specify a node version)
+* Download the script from this repository manually, or:
+  - If you don't have a `~/bin` folder, create it: `mkdir ~/bin` and close and reopen bash so bash will detect it and add it to the path. (The script doesn't _have_ to be installed in `~/bin`, I just found it convenient.)
+  - Run `curl https://raw.githubusercontent.com/CherryDT/wsl_node_network_interfaces_shim/master/wsl_node_network_interfaces_shim.sh > ~/bin/wsl_node_network_interfaces_shim.sh && chmod +x ~/bin/wsl_node_network_interfaces_shim.sh`
+* Install the shim: `sudo wsl_node_network_interfaces_shim.sh install`
+* Test it: `node -e 'console.log(require("os").networkInterfaces())'` (should output a loopback interface instead of failing)
+
+To uninstall the shim again, use: `sudo wsl_node_network_interfaces_shim.sh uninstall`
+In case anything goes wrong and the script left you in a state in which you can't use node, use: `sudo wsl_node_network_interfaces_shim.sh uninstall -f`
+
+By the way, the shim will always check whether the real `os.networkInterfaces` function fails before replacing it. If it does not fail (for example, you updated Windows to a build which fixes the problem natively), it will not replace it.
---
+++ wsl_node_network_interfaces_shim.sh
@@ -0,0 +1,226 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------
+# wsl_node_network_interfaces_shim.sh - Installs/uninstalls a shim to prevent
+# node from crashing when running in WSL when os.networkInterfaces is used
+
+# Copyright 2016, "David Trapp",,, <dt@david-trapp.com>
+  
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License at <http://www.gnu.org/licenses/> for
+# more details.
+
+# Usage: wsl_node_network_interfaces_shim.sh [-h|--help] [-f|--force]
+#        [install|uninstall]
+
+# Revision history:
+# 2016-12-29 Original version
+# ---------------------------------------------------------------------------
+
+PROGNAME=${0##*/}
+VERSION="0.1"
+
+echo "Network Interfaces shim for node.js under WSL"
+echo "Copyright 2016, David Trapp"
+echo
+
+clean_up() { # Perform pre-exit housekeeping
+  return
+}
+
+error_exit() {
+  echo -e "${PROGNAME}: ${1:-"Unknown Error"}" >&2
+  clean_up
+  exit 1
+}
+
+graceful_exit() {
+  clean_up
+  exit
+}
+
+signal_exit() { # Handle trapped signals
+  case $1 in
+    INT)
+      error_exit "Program interrupted by user" ;;
+    TERM)
+      echo -e "\n$PROGNAME: Program terminated" >&2
+      graceful_exit ;;
+    *)
+      error_exit "$PROGNAME: Terminating on unknown signal" ;;
+  esac
+}
+
+usage() {
+  echo -e "Usage: $PROGNAME [-h|--help] [-f|--force] [install|uninstall]"
+}
+
+help_message() {
+  cat <<- _EOF_
+  $PROGNAME ver. $VERSION
+  Installs/uninstalls a shim to prevent node from crashing when running in WSL
+  when os.networkInterfaces is used
+
+  $(usage)
+
+  Options:
+  -h, --help  Display this help message and exit.
+  -f, --force Ignore check for shim installation status - can be used with
+              "uninstall" to repair node symlink even if shim is broken
+  
+  Commands:
+  install     Install the shim
+  uninstall   Uninstall the shim
+
+  NOTE: You must be the superuser to run this script.
+
+_EOF_
+  return
+}
+
+# Trap signals
+trap "signal_exit TERM" TERM HUP
+trap "signal_exit INT"  INT
+
+# Is this WSL?
+if grep -q -v Microsoft /proc/sys/kernel/osrelease; then
+  error_exit "Environment appears not to be WSL!\nYou probably don't even need this script."
+fi
+
+# Check for root UID
+if [[ $(id -u) != 0 ]]; then
+  error_exit "You must be the superuser to run this script."
+fi
+
+# Check if node is already installed
+if [[ "$(which node)" == "/usr/bin/node" ]] && [[ "$(readlink $(which node))" == "/opt/wsl-node-shim/wsl-node-shim.sh" ]]; then
+  SHIM_INSTALLED=1
+else
+  SHIM_INSTALLED=0
+fi
+
+# Write the shim JS file
+write_shim_js() {
+
+cat << _EOF_
+
+var os = require('os');
+try {
+  os.networkInterfaces();
+} catch(e) {
+  os.networkInterfaces = function wslNodeShimNetworkInterfaces() {
+    return {
+      "Loopback Pseudo-Interface 1": [
+        {
+          address: '::1',
+          netmask: 'ffff:ffff:ffff:ffff:ffff:ffff:ffff:ffff',
+          family: 'IPv6',
+          mac: '00:00:00:00:00:00',
+          scopeid: 0,
+          internal: true
+        },
+        {
+          address: '127.0.0.1',
+          netmask: '255.0.0.0',
+          family: 'IPv4',
+          mac: '00:00:00:00:00:00',
+          internal: true
+        }
+      ]
+    };
+  };
+}
+_EOF_
+
+}
+
+# Write the shim SH file
+write_shim_sh() {
+
+cat << _EOF_
+#!/bin/bash
+/etc/alternatives/node -r /opt/wsl-node-shim/wsl-node-shim.js "\$@"
+_EOF_
+
+}
+
+# Parse command-line
+COMMAND=
+FORCE=0
+while [[ -n $1 ]]; do
+  case $1 in
+    -h | --help)
+      help_message; graceful_exit ;;
+    -f | --force)
+      FORCE=1 ;;
+    install)
+      COMMAND=install ;;
+    uninstall)
+      COMMAND=uninstall ;;
+    -* | --*)
+      usage
+      error_exit "Unknown option $1" ;;
+    *)
+      usage
+      error_exit "Unknown command $1" ;;
+  esac
+  shift
+done
+
+# Main logic
+case $COMMAND in
+  install)
+    if [[ $FORCE = 0 ]] && [[ $SHIM_INSTALLED = 1 ]]; then
+      error_exit "Shim is already installed."
+    fi
+    if [[ "$(which node)" != "/usr/bin/node" ]]; then
+      error_exit "node must be reachable via /usr/bin/node and\nin path, please use n for node version management.\nIf this message appears after an unsuccessful shim installation, use:\n$0 uninstall -f"
+    fi
+    if [[ "$(readlink $(which node))" != "/etc/alternatives/node" ]]; then
+      error_exit "/usr/bin/node must be linked to\n/etc/alternatives/node, please use n for node version management.\nIf this message appears after an unsuccessful shim installation, use:\n$0 uninstall -f"
+    fi
+    rm -rf /opt/wsl-node-shim
+    mkdir -p /opt/wsl-node-shim
+    write_shim_js > /opt/wsl-node-shim/wsl-node-shim.js
+    write_shim_sh > /opt/wsl-node-shim/wsl-node-shim.sh
+    chmod +x /opt/wsl-node-shim/wsl-node-shim.sh
+    rm /usr/bin/node
+    ln -s /opt/wsl-node-shim/wsl-node-shim.sh /usr/bin/node
+    echo "Shim installed!"
+    echo "os.networkInterfaces() will not crash anymore but return only a loopback"
+    echo "interface."
+    echo "To test, run: node -e 'console.log(require(\"os\").networkInterfaces())'"
+    ;;
+  uninstall)
+    if [[ $FORCE = 0 ]] && [[ $SHIM_INSTALLED = 0 ]]; then
+      error_exit "Shim is not installed."
+    fi
+    if [[ ! -e /etc/alternatives/node ]]; then
+      error_exit "/etc/alternatives/node must exist, please\nuse n for node version management."
+    fi
+    rm -rf /opt/wsl-node-shim
+    rm /usr/bin/node
+    ln -s /etc/alternatives/node /usr/bin/node
+    echo "Shim uninstalled!"
+    ;;
+  "")
+    if [[ $SHIM_INSTALLED = 1 ]]; then
+      echo "Shim is currently installed"
+    else
+      echo "Shim is currently NOT installed"
+    fi
+    echo
+    usage
+    ;;
+esac
+    
+
+
+graceful_exit
+
